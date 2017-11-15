aaffa257efebe4c0ad6ff33eedc45ad58fa36579
matyix <janos.matyas@gmail.com>
2017-11-15 19:45:35 +0100

    works

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/arm/examples/helpers"
 	"github.com/Azure/azure-sdk-for-go/arm/resources/resources"
@@ -69,16 +72,65 @@ func main() {
 	}
 
 	fmt.Printf("Token %#v", authenticatedToken.Token)
+
 	sdk.ServicePrincipal.AuthenticatedToken = authenticatedToken
 
 	resourceGroup := resources.NewGroupsClient(sdk.ServicePrincipal.SubscriptionID)
 	resourceGroup.Authorizer = autorest.NewBearerAuthorizer(sdk.ServicePrincipal.AuthenticatedToken)
 	sdk.ResourceGroup = &resourceGroup
 
-	fmt.Printf("Resource Group: %v#", sdk.ResourceGroup)
-
+	//Create resource group
+	/**
 	location := "eastus"
 	sdk.ResourceGroup.CreateOrUpdate("myRG1", resources.Group{Location: &location})
+	**/
+
+	//Autorest test
+	/**
+		GET /subscriptions/SUBSCRIPTION_ID/resourcegroups?api-version=2015-01-01 HTTP/1.1
+		Host: management.azure.com
+		Authorization: Bearer YOUR_ACCESS_TOKEN
+		Content-Type: application/json
+	**/
+
+	p := map[string]interface{}{"subscription-id": subscriptionID}
+	q := map[string]interface{}{"api-version": "2015-01-01"}
+
+	req, _ := autorest.Prepare(&http.Request{},
+		resourceGroup.WithAuthorization(),
+		autorest.AsGet(),
+		autorest.WithBaseURL("https://management.azure.com"),
+		autorest.WithPathParameters("/subscriptions/{subscription-id}/resourcegroups", p),
+		autorest.WithQueryParameters(q))
+
+	resp, err := autorest.SendWithSender(resourceGroup.Client, req)
+	if err != nil {
+		fmt.Errorf("SendWithSender %#v", err)
+		return
+	}
+
+	value := struct {
+		ResourceGroups []struct {
+			Name string `json:"name"`
+		} `json:"value"`
+	}{}
+
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&value)
+	if err != nil {
+		fmt.Errorf("Decode %#v", err)
+		return
+	}
+
+	var groupNames = make([]string, len(value.ResourceGroups))
+	fmt.Printf("Groups : %#v", groupNames)
+	for i, name := range value.ResourceGroups {
+		groupNames[i] = name.Name
+	}
+
+	fmt.Println("Groups:", strings.Join(groupNames, ", "))
 
 	return
+
 }
