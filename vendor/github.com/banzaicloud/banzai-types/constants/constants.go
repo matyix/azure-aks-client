4b5ee52dea34b14b8bef95a5b4436c9620dba29b
colin <kispiszkerf@gmail.com>
2018-04-05 17:43:13 +0200

    update vendor

--- vendor/github.com/banzaicloud/banzai-types/constants/constants.go
+++ vendor/github.com/banzaicloud/banzai-types/constants/constants.go
@@ -65,6 +65,8 @@ const (
 	Amazon = "amazon"
 	Azure  = "azure"
 	Google = "google"
+	Dummy  = "dummy"
+	BYOC   = "byoc"
 )
 
 // ### [ Constants to table names ] ### //
@@ -73,11 +75,15 @@ const (
 	TableNameAmazonProperties = "amazon_cluster_properties"
 	TableNameAzureProperties  = "azure_cluster_properties"
 	TableNameGoogleProperties = "google_cluster_properties"
+	TableNameDummyProperties  = "dummy_cluster_properties"
+	TableNameBYOCProperties   = "byoc_cluster_properties"
 )
 
 // ### [ Errors ] ### //
 var (
 	ErrorNotSupportedCloudType      = errors.New("Not supported cloud type")
+	ErrorAmazonClusterNameRegexp    = errors.New("Up to 255 letters (uppercase and lowercase), numbers, hyphens, and underscores are allowed.")
+	ErrorGoogleClusterNameRegexp    = errors.New("Name must start with a lowercase letter followed by up to 40 lowercase letters, numbers, or hyphens, and cannot end with a hyphen.")
 	ErrorAzureClusterNameRegexp     = errors.New("Only numbers, lowercase letters and underscores are allowed under name property. In addition, the value cannot end with an underscore, and must also be less than 32 characters long.")
 	ErrorAzureClusterNameEmpty      = errors.New("The name should not be empty.")
 	ErrorAzureClusterNameTooLong    = errors.New("Cluster name is greater than or equal 32")
@@ -89,4 +95,13 @@ var (
 	ErrorNilCluster                 = errors.New("<nil> cluster")
 	ErrorWrongKubernetesVersion     = errors.New("Wrong kubernetes version for master/nodes. The required minimum kubernetes version is 1.8.x ")
 	ErrorDifferentKubernetesVersion = errors.New("Different kubernetes version for master and nodes")
+	ErrorLocationEmpty              = errors.New("Location field is empty")
+	ErrorNodeInstanceTypeEmpty      = errors.New("NodeInstanceType field is empty")
+)
+
+// ### [ Regexps for cluster names ] ### //
+const (
+	RegexpAWSName = `^[A-z0-9-_]{1,255}$`
+	RegexpAKSName = `^[a-z0-9_]{0,31}[a-z0-9]$`
+	RegexpGKEName = `^[a-z]$|^[a-z][a-z0-9-]{0,38}[a-z0-9]$`
 )
